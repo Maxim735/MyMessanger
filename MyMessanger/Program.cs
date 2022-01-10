bc1efd2d348bf12047f6d387675d402a2b835532
Max1mKa <Maksiksay@gmail.com>
2022-01-10 08:46:18 +0300

    Added the Message class

---
+++ MyMessanger/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMessanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(msg.ToString());
+        }
+    }
+}
