8ed22cdddb69ad2fd7a869bc90af2ee4ba8f3547
Max1mKa <Maksiksay@gmail.com>
2022-01-15 16:01:16 +0300

    an asynchronous method for processing messages has been added to the client, and the output of messages to the application interface is also ready

--- WindowsFormsClient/Form1.Designer.cs
+++ WindowsFormsClient/Form1.Designer.cs
@@ -29,53 +29,99 @@ namespace WindowsFormsClient
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.MessagesLB = new System.Windows.Forms.ListBox();
+            this.UsernameTB = new System.Windows.Forms.TextBox();
+            this.MessageTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // button1
+            // SendButton
             // 
-            this.button1.Location = new System.Drawing.Point(599, 330);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 57);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.SendButton.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendButton.Location = new System.Drawing.Point(621, 350);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(128, 57);
+            this.SendButton.TabIndex = 0;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
             // 
-            // listBox1
+            // MessagesLB
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(0, 2);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(666, 308);
-            this.listBox1.TabIndex = 1;
+            this.MessagesLB.FormattingEnabled = true;
+            this.MessagesLB.ItemHeight = 16;
+            this.MessagesLB.Location = new System.Drawing.Point(0, 2);
+            this.MessagesLB.Name = "MessagesLB";
+            this.MessagesLB.Size = new System.Drawing.Size(666, 308);
+            this.MessagesLB.TabIndex = 1;
+            this.MessagesLB.SelectedIndexChanged += new System.EventHandler(this.Messages_SelectedIndexChanged);
             // 
-            // textBox1
+            // UsernameTB
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 330);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(341, 22);
-            this.textBox1.TabIndex = 2;
+            this.UsernameTB.Location = new System.Drawing.Point(16, 338);
+            this.UsernameTB.Name = "UsernameTB";
+            this.UsernameTB.Size = new System.Drawing.Size(341, 22);
+            this.UsernameTB.TabIndex = 2;
+            this.UsernameTB.Tag = "u";
+            this.UsernameTB.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // MessageTB
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 364);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(580, 22);
-            this.textBox2.TabIndex = 3;
+            this.MessageTB.Location = new System.Drawing.Point(16, 385);
+            this.MessageTB.Name = "MessageTB";
+            this.MessageTB.Size = new System.Drawing.Size(580, 22);
+            this.MessageTB.TabIndex = 3;
+            this.MessageTB.Text = "Enter a text...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MessageTB);
+            this.Controls.Add(this.UsernameTB);
+            this.Controls.Add(this.MessagesLB);
+            this.Controls.Add(this.SendButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -85,10 +131,14 @@ namespace WindowsFormsClient
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ListBox MessagesLB;
+        private System.Windows.Forms.TextBox UsernameTB;
+        private System.Windows.Forms.TextBox MessageTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WindowsFormsClient/Form1.cs
+++ WindowsFormsClient/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyMessanger;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft;
 
 namespace WindowsFormsClient
 {
     public partial class Form1 : Form
     {
+        private static int MessageID = 0;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Messages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            string UserName = UsernameTB.Text;
+            string Message = MessageTB.Text;
+            if(UserName.Length > 1)
+            {
+                MyMessanger.Message msg = new MyMessanger.Message(UserName, Message, DateTime.Now);
+                API.SendMessage(msg);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var getMessage = new Func<Task>(async () =>
+            {
+                MyMessanger.Message msg = await API.GetMessageHTTPAsync(MessageID);
+                while (msg != null)
+                {
+                    MessagesLB.Items.Add(msg);
+                    MessageID++;
+                    msg = await API.GetMessageHTTPAsync(MessageID);
+                }
+            });
+            getMessage.Invoke();
         }
     }
 }
